ac8f6f9322d853e87c798866eb7297d612f788cd
Leonard <leo@leonard-lemke.com>
2023-01-30 19:18:42 +0100

    Add QSTile service, detail view for Quick Settings
    - Add a QSTile service to the AndroidManifest.xml file
    - Add a QSTile class for Quick Settings Tile
    - Add a new layout file for the Quick Settings detail view
    - Add strings for QS Detail View and QS Tile Subtitle in multiple languages
    - Add logic for setting the tile state to active or inactive on click
    - Add logic for updating the tile subtitle when in listening state
    - Add logic for Samsung devices to show a detail view for the
    
    [app/src/main/res/layout/qs_detail_view.xml]
    - Add a new layout file for the Quick Settings detail view
    - Set the layout width and height to match parent
    - Set the orientation to vertical
    - Add a TextView with ID `qs_detail_text`
    - Set the TextView width and height to match parent
    - Set the text alignment to center
    - Set the gravity to center
    - Set the layout margin to 24dp
    - Set the text to the string `qs_detail_view_text`
    [app/src/main/AndroidManifest.xml]
    - Add a QSTile service to the AndroidManifest.xml file
    - Add exported, icon, label and permission attributes to the QSTile service
    - Add an intent-filter action for the QSTile service
    [app/src/main/java/dev/oneuiproject/oneui/oneuisampleapp/ui/QSTile.kt]
    - Add QSTile class for Quick Settings Tile
    - Add onCreate, onStartListening, onStopListening, and onClick methods
    - Add logic for setting the tile state to active or inactive on click
    - Add logic for updating the tile subtitle when in listening state
    - Add logic for Samsung devices to show a detail view for the tile with a toggle button and a setting button
    [app/src/main/res/values/strings.xml]
    - Add strings for QS Detail View and QS Tile Subtitle
    [app/src/main/res/values-de/strings.xml]
    - Add a string for QS detail view
    - Add a string for QS tile subtitle

---
+++ app/src/main/java/dev/oneuiproject/oneui/oneuisampleapp/ui/QSTile.kt
@@ -0,0 +1,63 @@
+package dev.oneuiproject.oneui.oneuisampleapp.ui
+
+import android.content.Intent
+import android.os.Build
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+import android.widget.RemoteViews
+import android.widget.Toast
+import dev.oneuiproject.oneui.oneuisampleapp.R
+
+class QSTile : TileService() {
+
+    override fun onCreate() {
+        //Called by the system when the service is first created.
+        super.onCreate()
+    }
+
+    override fun onStartListening() {
+        //Called when this tile moves into a listening state.
+        //When this tile is in a listening state it is expected to keep the UI up to date.
+        //Any listeners or callbacks needed to keep this tile up to date should be registered here.
+        super.onStartListening()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            qsTile.subtitle = getString(R.string.qs_tile_subtitle)
+            qsTile.updateTile()
+        }
+    }
+
+    override fun onStopListening() {
+        //Called when this tile moves out of a listening state.
+        //Any listeners or callbacks registered in onStartListening() should be unregistered here.
+        super.onStopListening()
+    }
+
+    override fun onClick() {
+        super.onClick()
+        if (qsTile.state == Tile.STATE_ACTIVE) {
+            qsTile.state = Tile.STATE_INACTIVE
+            Toast.makeText(this, "Tile clicked: inactive", Toast.LENGTH_SHORT).show()
+        } else {
+            qsTile.state = Tile.STATE_ACTIVE
+            Toast.makeText(this, "Tile clicked: active", Toast.LENGTH_SHORT).show()
+        }
+        qsTile.updateTile()
+    }
+
+
+    /** #### Detail view (samsung only) #### **/
+
+    fun semGetSettingsIntent(): Intent = Intent(this, MainActivity::class.java)
+    fun semIsToggleButtonExists(): Boolean = true
+    fun semIsToggleButtonChecked(): Boolean = qsTile.state == Tile.STATE_ACTIVE
+    fun semGetDetailViewTitle(): CharSequence = this.getString(R.string.app_name)
+    fun semGetDetailView(): RemoteViews = RemoteViews(packageName, R.layout.qs_detail_view)
+    fun semSetToggleButtonChecked(checked: Boolean) {
+        Toast.makeText(this, "Toggle Button: $checked", Toast.LENGTH_SHORT).show()
+        qsTile.state = if (checked) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+        qsTile.updateTile()
+    }
+
+    fun semGetDetailViewSettingButtonName(): CharSequence = "Setting Button Name" //unknown
+
+}
