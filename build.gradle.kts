ddb329e54439d3759dcd5e1a3b465c7399d501d7
Leonard <leo@leonard-lemke.com>
2024-01-26 16:42:34 +0100

    Upgrade AGP dependency from 8.2.1 to 8.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.1")
+        classpath("com.android.tools.build:gradle:8.2.2")
         //classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.22-1.0.16")
     }
 }
@@ -20,8 +20,8 @@ allprojects {
 }
 
 plugins {
-    id("com.android.application") version "8.2.1" apply false
-    id("com.android.library") version "8.2.1" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
     id("com.google.dagger.hilt.android") version "2.42" apply false
 }
