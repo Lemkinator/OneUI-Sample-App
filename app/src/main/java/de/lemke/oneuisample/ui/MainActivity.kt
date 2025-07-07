df15e4383a9defe368bc376ad8f520780b6f7a9b
Leonard <leo@leonard-lemke.com>
2025-07-07 21:10:24 +0200

    Enhance splash screen with custom exit animation
    This commit introduces a custom exit animation for the splash screen in `MainActivity`. The new animation includes:
    
    - Fading out the splash screen icon while slightly scaling it up.
    - Fading in the main activity content while scaling it down from a slightly larger size.
    - Using an `AccelerateDecelerateInterpolator` for a smoother transition.
    - Ensuring the animation starts after the default splash icon animation duration has passed.
    
    Additionally, the logic for opening the OOBE (Out-of-Box Experience) activity from the navigation drawer has been extracted into a separate private function `openOOBEAndFinish()` for better code organization.

--- app/src/main/java/de/lemke/oneuisample/ui/MainActivity.kt
+++ app/src/main/java/de/lemke/oneuisample/ui/MainActivity.kt
@@ -2,11 +2,18 @@ package de.lemke.oneuisample.ui
 
 import android.R.anim.fade_in
 import android.R.anim.fade_out
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.content.Intent
 import android.content.Intent.ACTION_SEARCH
 import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
+import android.view.View.ALPHA
+import android.view.View.SCALE_X
+import android.view.View.SCALE_Y
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.appbar.model.ButtonModel
@@ -23,7 +30,9 @@ import de.lemke.oneuisample.ui.util.onNavigationSingleClick
 import de.lemke.oneuisample.ui.util.setupHeaderAndNavRail
 import de.lemke.oneuisample.ui.util.suggestiveSnackBar
 import dev.oneuiproject.oneui.navigation.setupNavigation
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.lang.System.currentTimeMillis
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -48,6 +57,32 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         splashScreen.setKeepOnScreenCondition { !isUIReady }
+        splashScreen.setOnExitAnimationListener { splash ->
+            val splashAnimator: ObjectAnimator = ObjectAnimator.ofPropertyValuesHolder(
+                splash.view,
+                PropertyValuesHolder.ofFloat(ALPHA, 0f),
+                PropertyValuesHolder.ofFloat(SCALE_X, 1.2f),
+                PropertyValuesHolder.ofFloat(SCALE_Y, 1.2f)
+            )
+            splashAnimator.interpolator = AccelerateDecelerateInterpolator()
+            splashAnimator.duration = 400L
+            splashAnimator.doOnEnd { splash.remove() }
+            val contentAnimator: ObjectAnimator = ObjectAnimator.ofPropertyValuesHolder(
+                binding.root,
+                PropertyValuesHolder.ofFloat(ALPHA, 0f, 1f),
+                PropertyValuesHolder.ofFloat(SCALE_X, 1.2f, 1f),
+                PropertyValuesHolder.ofFloat(SCALE_Y, 1.2f, 1f)
+            )
+            contentAnimator.interpolator = AccelerateDecelerateInterpolator()
+            contentAnimator.duration = 400L
+            val remainingDuration =
+                splash.iconAnimationDurationMillis - (currentTimeMillis() - splash.iconAnimationStartMillis).coerceAtLeast(0L)
+            lifecycleScope.launch {
+                delay(remainingDuration)
+                splashAnimator.start()
+                contentAnimator.start()
+            }
+        }
         lifecycleScope.launch {
             when (checkAppStart()) {
                 AppStart.FIRST_TIME -> openOOBE()
@@ -85,12 +120,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private fun initDrawer() {
         binding.navigationView.onNavigationSingleClick { item ->
             when (item.itemId) {
-                R.id.oobe_dest -> {
-                    startActivity(Intent(this@MainActivity, OOBEActivity::class.java))
-                    @Suppress("DEPRECATION") if (SDK_INT < 34) overridePendingTransition(fade_in, fade_out)
-                    finishAfterTransition()
-                }
-
+                R.id.oobe_dest -> openOOBEAndFinish()
                 R.id.about_app_dest -> startActivity(Intent(this, AboutActivity::class.java))
                 R.id.about_custom_dest -> startActivity(Intent(this, CustomAboutActivity::class.java))
                 R.id.settings_dest -> startActivity(Intent(this, SettingsActivity::class.java))
@@ -105,6 +135,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             //isImmersiveScroll = true
             setupNavigation(binding.bottomTab, binding.navigationHost.getFragment())
         }
+    }
+
+    private fun openOOBEAndFinish() {
+        startActivity(Intent(this@MainActivity, OOBEActivity::class.java))
+        @Suppress("DEPRECATION") if (SDK_INT < 34) overridePendingTransition(fade_in, fade_out)
+        finishAfterTransition()
     }
 
     private fun createSuggestAppBarModel(): SuggestAppBarModel<SuggestAppBarView> =
