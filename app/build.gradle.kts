fbe9b28f138f848ea8aa7f1f118f7446aef73986
Leonard <leo@leonard-lemke.com>
2025-07-20 19:07:38 +0200

    Remove obsolete SESL Jetpack and AndroidX libraries
    The oneui-design library now includes these, making separate declarations redundant. This change simplifies the dependency graph.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,33 +84,12 @@ android {
 }
 
 dependencies {
-    //SESL Android Jetpack
-    implementation("sesl.androidx.core:core:1.17.0-beta01+1.0.7-sesl8+rev0")
-    implementation("sesl.androidx.core:core-ktx:1.17.0-beta01+1.0.0-sesl8+rev0")
-    implementation("sesl.androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01+1.0.0-sesl8+rev0")
-    implementation("sesl.androidx.fragment:fragment:1.8.8+1.0.5-sesl8+rev0")
-    implementation("sesl.androidx.appcompat:appcompat:1.7.1+1.0.18-sesl8+rev0")
-    implementation("sesl.androidx.recyclerview:recyclerview:1.4.0+1.0.12-sesl8+rev0")
-    implementation("sesl.androidx.picker:picker-basic:1.0.7+1.0.7-sesl8+rev0")
-    implementation("sesl.androidx.picker:picker-app:1.0.6+1.0.6-sesl8+rev0")
-    implementation("sesl.androidx.indexscroll:indexscroll:1.0.0+1.0.0-sesl8+rev0")
-    implementation("sesl.androidx.preference:preference:1.2.1+1.0.0-sesl8+rev0")
-    implementation("sesl.androidx.viewpager2:viewpager2:1.1.0+1.0.0-sesl8+rev0")
-    implementation("sesl.androidx.picker:picker-color:1.0.2+1.0.2-sesl8+rev0")
-    //SESL Material Components + Design Lib + Icons
-    implementation("sesl.com.google.android.material:material:1.12.0+1.0.31-sesl8+rev1")
     implementation("io.github.tribalfs:oneui-design:0.7.6+oneui7")
     implementation("io.github.oneuiproject:icons:1.1.0")
-
     implementation("com.airbnb.android:lottie:6.6.7")
     implementation("com.google.android.gms:play-services-oss-licenses:17.2.1")
     implementation("androidx.core:core-splashscreen:1.2.0-rc01")
     implementation("androidx.datastore:datastore-preferences:1.1.7")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
-    val navVersion = "2.9.2"
-    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
-    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
-
     implementation("com.google.dagger:hilt-android:2.57")
     ksp("com.google.dagger:hilt-compiler:2.57")
 }
@@ -130,7 +109,6 @@ configurations.implementation {
     exclude ("androidx.recyclerview", "recyclerview")
     exclude ("androidx.slidingpanelayout", "slidingpanelayout")
     exclude ("androidx.swiperefreshlayout", "swiperefreshlayout")
-
     //Exclude official material components lib
     exclude ("com.google.android.material", "material")
 }
