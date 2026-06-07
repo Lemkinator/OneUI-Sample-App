90e4f0fc0bd0bfda41a3e676256118064df2baec
Leonard <leo@leonard-lemke.com>
2026-06-07 14:05:53 +0200

    refactor: simplify FragmentBottomSheet by using layout ID constructor
    - Remove manual ViewBinding and `onCreateView` implementation.
    - Pass `R.layout.fragment_bottom_sheet` directly to the `SemBottomSheetDialogFragment` constructor.

--- app/src/main/java/de/lemke/oneuisample/ui/fragments/FragmentBottomSheet.kt
+++ app/src/main/java/de/lemke/oneuisample/ui/fragments/FragmentBottomSheet.kt
@@ -2,29 +2,17 @@ package de.lemke.oneuisample.ui.fragments
 
 import android.app.Dialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_EXPANDED
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import dagger.hilt.android.AndroidEntryPoint
-import de.lemke.oneuisample.databinding.FragmentBottomSheetBinding
-import de.lemke.oneuisample.ui.util.autoCleared
+import de.lemke.oneuisample.R
 import dev.oneuiproject.oneui.app.SemBottomSheetDialogFragment
 
 @AndroidEntryPoint
-class FragmentBottomSheet : SemBottomSheetDialogFragment() {
-    private val binding by autoCleared { FragmentBottomSheetBinding.bind(requireView()) }
-
+class FragmentBottomSheet : SemBottomSheetDialogFragment(R.layout.fragment_bottom_sheet) {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
         (super.onCreateDialog(savedInstanceState) as BottomSheetDialog).apply {
             behavior.skipCollapsed = true
             setOnShowListener { behavior.state = STATE_EXPANDED }
         }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View = FragmentBottomSheetBinding.inflate(inflater, container, false).root
 }
