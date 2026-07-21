31d0c1eb274514b1da49e2856d555d44917d7d46
Leonard <leo@leonard-lemke.com>
2026-07-21 14:35:23 +0200

    test: use fakeUserSettings() in AboutViewModelTest

--- app/src/test/java/de/lemke/oneuisample/ui/AboutViewModelTest.kt
+++ app/src/test/java/de/lemke/oneuisample/ui/AboutViewModelTest.kt
@@ -15,22 +15,18 @@
  */
 package de.lemke.oneuisample.ui
 
-import de.lemke.oneuisample.data.FakeSharedPreferences
 import de.lemke.oneuisample.data.UserSettings
+import de.lemke.oneuisample.data.fakeUserSettings
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
 
-@OptIn(ExperimentalCoroutinesApi::class)
 class AboutViewModelTest : ShouldSpec(
     {
         lateinit var settings: UserSettings
         lateinit var viewModel: AboutViewModel
 
         beforeEach {
-            settings = UserSettings(FakeSharedPreferences(), CoroutineScope(UnconfinedTestDispatcher()))
+            settings = fakeUserSettings()
             viewModel = AboutViewModel(settings)
         }
 
