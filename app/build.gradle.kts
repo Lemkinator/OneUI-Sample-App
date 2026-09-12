4017fae5dc647b9006891abcea8144958a5635a5
Leonard <leo@leonard-lemke.com>
2026-09-12 18:10:56 +0200

    bump: update versionCode to 2 and versionName to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,8 @@ android {
             libs.versions.targetSdk
                 .get()
                 .toInt()
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         testInstrumentationRunner = "de.lemke.oneuisample.HiltTestRunner"
         buildConfigField("boolean", "FIRST_RUN_SKIPPABLE", "false")
     }
